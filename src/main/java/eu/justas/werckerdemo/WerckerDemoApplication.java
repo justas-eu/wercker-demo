362eb960ce0eab8eb2b3f1f51621005f61fd05dc
Justas Sperauskas <justas@justas.eu>
2018-07-31 21:08:49 +0300

    Spring boot intlrz bootstarted project

---
+++ src/main/java/eu/justas/werckerdemo/WerckerDemoApplication.java
@@ -0,0 +1,12 @@
+package eu.justas.werckerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WerckerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WerckerDemoApplication.class, args);
+	}
+}
